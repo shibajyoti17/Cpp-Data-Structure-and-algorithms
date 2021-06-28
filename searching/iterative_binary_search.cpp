662778bae47b6e77d1643182921eb16d71b28780
shibajyoti17 <shibajyotim@gmail.com>
2021-06-28 14:23:29 +0530

    Changed the iterative binary search program

--- searching/iterative_binary_search.cpp
+++ searching/iterative_binary_search.cpp
@@ -1,3 +1,10 @@
+/* The program is used to search an element in a fixed size array using
+   binary search.
+   
+   Inputs: -> The item which need to be searched.
+   Output: -> Returns the index(1-based) of the element in the fixed size array
+*/
+// 06-28-21 Fixed the array order to sorted, since this is a binary search.
 #include<iostream>
 using namespace std;
 int binarySearch(int arr[], int item, int arr_size) {
@@ -17,8 +24,13 @@ int binarySearch(int arr[], int item, int arr_size) {
     return -1;
 }
 int main() {
-    int arr[7] = {2,67,43,34,21,643,59};
+    int arr[7] = {2,43,65,120,170,190,220};
     int arr_size = sizeof(arr) / sizeof(int);
+    cout << "The fixed size array is: " << endl;
+    for(int i = 0; i < arr_size; i++){
+	cout << arr[i] << " ";
+    }
+    cout << "\n" << "Enter the item which you want to search: " << endl;
     int item;
     cin >> item;
     cout << "Index of Element: " << binarySearch(arr,item,arr_size) + 1 << endl;
